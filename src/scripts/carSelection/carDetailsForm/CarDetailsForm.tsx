29ca0df145a4ba82993b97c515819e27b8b06db2
mizeligo <michalina.zeligowska@assecobs.pl>
2021-11-04 16:58:43 +0100

    car details form todo

---
+++ src/scripts/carSelection/carDetailsForm/CarDetailsForm.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CarDetailsForm = (): React.ReactElement => {
+    return <div></div>; // dropdowny z cechami samochodu do wyboru i search -> wyswietlenie karty samochodu
+};
